15b280f78f6da0fb06367e0f1cffb9ef9410dff8
abdohosny1 <make.y90@yahoo.com>
2022-07-01 14:33:03 +0200

    add basket api

---
+++ src/Basket/BasketApii/Repositories/interfaces/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using BasketApii.Entities;
+using System.Threading.Tasks;
+
+namespace BasketApii.Repositories.interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
